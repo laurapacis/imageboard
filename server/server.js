c35e2fb9074be59a5376300e179d6c49d8bd4c51
limononoil <63930000+limononoil@users.noreply.github.com>
2021-03-26 13:20:12 +0100

    setup server and connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,4 +1,39 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+const app = express();
+
+dotenv.config();
+
+// generate setup
+app.use(express.json({ limit: '30mb', extended: true}));
+app.use(express.urlencoded({ limit: '30mb, extended: true' }));
+app.use(cors());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectionStr = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${MONGO_URI}/${DB_NAME}?retryWrites=true&w=majority`;
+
+// connect to MongoDB
+mongoose.connect(connectionStr, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+    .then(() => {
+        console.log(`MongoDB is connected 😎 on http://localhost:${PORT}`);
+    })
+    .catch((error) => {
+        console.log("[ERROR] DB Connection failed", error);
+    });
