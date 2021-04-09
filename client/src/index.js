2b9c6147fc2eb9fdfa0c6428072a8292b53105d9
limononoil <63930000+limononoil@users.noreply.github.com>
2021-04-09 15:25:18 +0200

    set up redux

--- client/src/index.js
+++ client/src/index.js
@@ -10,4 +10,8 @@ import App from './App';
 
 const store = createStore(reducers, compose(applyMiddelware(thunk)))
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
