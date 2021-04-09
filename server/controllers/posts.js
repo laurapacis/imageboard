b5fb2eaefb6fef30ebe787c118138c1acc7f3260
limononoil <63930000+limononoil@users.noreply.github.com>
2021-04-09 13:21:36 +0200

    :sparkles:add frontend components structure + :art:start styling with materialUI

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,9 +3,9 @@ import PostMessage from "../models/PostMessage.js";
 export const getPosts = async (req, res, next) => {
     try {
         const allPostMessages = await PostMessage.find();
-        res.status(200).json(allPostMessages);
+        res.status(200).json(allPostMessages); // 200 = "OK"
     } catch (error) {
-        next(res.status(404), json({ message: error.message }));
+        next(res.status(404).json({ message: error.message })); // 404 => "Not Found"
     }
 };
 
@@ -15,8 +15,8 @@ export const createPost = async (req, res, next) => {
     try {
         const newPost = await PostMessage.create(post);
         console.log(newPost);
-        res.status(201).json(newPost);
+        res.status(201).json(newPost); // 201 = "Created"
     } catch (error) {
-        next(res.status(404), json({ message: error.message }));
+        next(res.status(404).json({ message: error.message }));
     }
 };
