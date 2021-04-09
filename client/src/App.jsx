a71f8b703e04ad233d21dec1cbf54624c2d3ce8a
limononoil <63930000+limononoil@users.noreply.github.com>
2021-04-09 15:10:50 +0200

    :sparkles:add frontend components structure + :art:start styling with materialUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import cherub from './images/Cherub.jpg';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import useStyles from "./styles";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Imageboard
+        </Typography>
+        <img className={classes.image} src={cherub} alt="cherub" height="60"/>
+      </AppBar>
+
+      <Grow in>
+
+        <Container>
+          
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            
+            <Grid item xz={12} sm={4}>
+              <Form/>
+            </Grid>
+
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddelware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
 import App from './App';
+
+const store = createStore(reducers, compose(applyMiddelware(thunk)))
 
 ReactDOM.render(<App />, document.getElementById('root'));
